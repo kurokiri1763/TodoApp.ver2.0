5c4a0d386e1f7b7d55f9e64a13c5985d7cf3f969
kurokiri1763 <kurokiri1763@gmail.com>
2025-02-17 23:53:57 +0900

    UI追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import localforage from 'localforage';
+import { useState } from 'react';
+
 import GlobalStyles from '@mui/material/GlobalStyles';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { indigo, pink } from '@mui/material/colors';
+
 import { ToolBar } from './ToolBar';
-import { useEffect,useState } from 'react';
-import { isTodos } from './lib/isTodos';
-import { FormDialog } from './FormDialog';
-import { ActionButton } from './ActionButton';
 import { SideBar } from './SideBar';
 import { TodoItem } from './TodoItem';
+import { FormDialog } from './FormDialog';
+import { ActionButton } from './ActionButton';
 
 const theme = createTheme({
   palette: {
     primary: {
       main: indigo[500],
-      light: '757de8',
+      light: '#757de8',
       dark: '#002984',
     },
     secondary: {
@@ -26,84 +26,69 @@ const theme = createTheme({
 });
 
 export const App = () => {
-  const [text,setText] = useState('');
+  const [text, setText] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [filter,setFilter] = useState<Filter>('all');
+  const [filter, setFilter] = useState<Filter>('all');
 
   const [drawerOpen, setDrawerOpen] = useState(false);
+
   const handleToggleDrawer = () => {
     setDrawerOpen((drawerOpen) => !drawerOpen);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
-  }
+  };
 
-  // Todo作成処理
   const handleSubmit = () => {
     if (!text) return;
 
-    // Todo型に限定
     const newTodo: Todo = {
-      value : text,
-      id: new Date ().getTime(),
+      value: text,
+      id: new Date().getTime(),
       checked: false,
       removed: false,
     };
 
     setTodos((todos) => [newTodo, ...todos]);
     setText('');
-    };
+  };
 
-    const handleTodo = <K extends keyof Todo, V extends Todo[K]>(
-      id: number,
-      key: K,
-      value: V,
-    ) => {
-      setTodos((todos) => {
-        const newTodos = todos.map((todo) => {
-          if (todo.id === id) {
-            return {...todo, [key]: value };
-          }
-          else {
-            return todo;
-          }
-        });
-        return newTodos
+  const handleTodo = <K extends keyof Todo, V extends Todo[K]>(
+    id: number,
+    key: K,
+    value: V
+  ) => {
+    setTodos((todos) => {
+      const newTodos = todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, [key]: value };
+        } else {
+          return todo;
+        }
       });
-    };
 
-  // フィルター処理
-  const handleFilter = (filter: Filter) => {
-    setFilter(filter);
+      return newTodos;
+    });
   };
 
   const handleEmpty = () => {
     setTodos((todos) => todos.filter((todo) => !todo.removed));
-  };  
+  };
 
-    /**
-   * キー名 'todo-20200101' のデータを取得
-   * 第 2 引数の配列が空なのでコンポーネントのマウント時のみに実行される
-  */
-    useEffect(() => {
-      localforage
-        .getItem('todo-20200101')
-        .then((values) => isTodos(values) && setTodos(values as Todo[]));
-    }, []);
-  
-    /**
-     * todos ステートが更新されたら、その値を保存
-    */
-    useEffect(() => {
-      localforage.setItem('todo-20200101', todos);
-    }, [todos]);
+  const handleFilter = (filter: Filter) => {
+    setFilter(filter);
+  };
 
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles styles={{ body: {margin :0, padding: 0 } }} />
-      <ToolBar filter={filter} />
-      <SideBar onFilter={handleFilter} />
+      <GlobalStyles styles={{ body: { margin: 0, padding: 0 } }} />
+      <ToolBar filter={filter} onToggleDrawer={handleToggleDrawer} />
+      <SideBar
+        drawerOpen={drawerOpen}
+        onFilter={handleFilter}
+        onToggleDrawer={handleToggleDrawer}
+      />
       <FormDialog text={text} onChange={handleChange} onSubmit={handleSubmit} />
       <TodoItem todos={todos} filter={filter} onTodo={handleTodo} />
       <ActionButton todos={todos} onEmpty={handleEmpty} />
--- src/ToolBar.tsx
+++ src/ToolBar.tsx
@@ -6,39 +6,41 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 
 type Props = {
-    filter: Filter;
+  filter: Filter;
+  onToggleDrawer: () => void;
 };
 
 const translator = (arg: Filter) => {
-    switch (arg) {
-        case 'all':
-            return '全てのタスク';
-        case 'unchecked':
-            return '現在のタスク';
-        case 'checked':
-            return '完了したタスク';
-        case 'removed':
-            return 'ゴミ箱';
-        default:
-            return 'TODO';
-    }
+  switch (arg) {
+    case 'all':
+      return 'すべてのタスク';
+    case 'unchecked':
+      return '現在のタスク';
+    case 'checked':
+      return '完了したタスク';
+    case 'removed':
+      return 'ごみ箱';
+    default:
+      return 'TODO';
+  }
 };
 
 export const ToolBar = (props: Props) => (
-    <Box sx={{flexGrow: 1}}>
-        <AppBar position="static">
-            <Toolbar>
-                <IconButton
-                    aria-label="menu-button"
-                    size="large"
-                    edge="start"
-                    color="inherit"
-                    sx={{ mr: 2}}
-                >
-                    <Icon>menu</Icon>
-                </IconButton>
-                <Typography>{translator(props.filter)}</Typography>
-            </Toolbar>
-        </AppBar>
-    </Box>
+  <Box sx={{ flexGrow: 1 }}>
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton
+          aria-label="menu-button"
+          size="large"
+          edge="start"
+          color="inherit"
+          sx={{ mr: 2 }}
+          onClick={props.onToggleDrawer}
+        >
+          <Icon>menu</Icon>
+        </IconButton>
+        <Typography>{translator(props.filter)}</Typography>
+      </Toolbar>
+    </AppBar>
+  </Box>
 );
