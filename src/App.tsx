70df64cc8ca41c8ac2ea4ef42cddd66c5c4d4571
kurokiri1763 <kurokiri1763@gmail.com>
2025-02-18 14:16:19 +0900

    ツールバー実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import { useState } from 'react';
-
+import localforage from 'localforage';
 import GlobalStyles from '@mui/material/GlobalStyles';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { indigo, pink } from '@mui/material/colors';
-
 import { ToolBar } from './ToolBar';
-import { SideBar } from './SideBar';
-import { TodoItem } from './TodoItem';
+import { useEffect,useState } from 'react';
+import { isTodos } from './lib/isTodos';
 import { FormDialog } from './FormDialog';
 import { ActionButton } from './ActionButton';
+import { SideBar } from './SideBar';
+import { TodoItem } from './TodoItem';
 
 const theme = createTheme({
   palette: {
     primary: {
       main: indigo[500],
-      light: '#757de8',
+      light: '757de8',
       dark: '#002984',
     },
     secondary: {
@@ -26,69 +26,82 @@ const theme = createTheme({
 });
 
 export const App = () => {
-  const [text, setText] = useState('');
+  const [text,setText] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
-  const [filter, setFilter] = useState<Filter>('all');
+  const [filter,setFilter] = useState<Filter>('all');
 
   const [drawerOpen, setDrawerOpen] = useState(false);
-
   const handleToggleDrawer = () => {
     setDrawerOpen((drawerOpen) => !drawerOpen);
   };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
-  };
+  }
 
   const handleSubmit = () => {
     if (!text) return;
 
     const newTodo: Todo = {
-      value: text,
-      id: new Date().getTime(),
+      value : text,
+      id: new Date ().getTime(),
       checked: false,
       removed: false,
     };
 
     setTodos((todos) => [newTodo, ...todos]);
     setText('');
-  };
+    };
 
-  const handleTodo = <K extends keyof Todo, V extends Todo[K]>(
-    id: number,
-    key: K,
-    value: V
-  ) => {
-    setTodos((todos) => {
-      const newTodos = todos.map((todo) => {
-        if (todo.id === id) {
-          return { ...todo, [key]: value };
-        } else {
-          return todo;
-        }
+    const handleTodo = <K extends keyof Todo, V extends Todo[K]>(
+      id: number,
+      key: K,
+      value: V,
+    ) => {
+      setTodos((todos) => {
+        const newTodos = todos.map((todo) => {
+          if (todo.id === id) {
+            return {...todo, [key]: value };
+          }
+          else {
+            return todo;
+          }
+        });
+        return newTodos
       });
+    };
 
-      return newTodos;
-    });
-  };
-
-  const handleEmpty = () => {
-    setTodos((todos) => todos.filter((todo) => !todo.removed));
-  };
-
+  // フィルター処理
   const handleFilter = (filter: Filter) => {
     setFilter(filter);
   };
 
+  const handleEmpty = () => {
+    setTodos((todos) => todos.filter((todo) => !todo.removed));
+  };  
+
+    /**
+   * キー名 'todo-20200101' のデータを取得
+   * 第 2 引数の配列が空なのでコンポーネントのマウント時のみに実行される
+  */
+    useEffect(() => {
+      localforage
+        .getItem('todo-20200101')
+        .then((values) => isTodos(values) && setTodos(values as Todo[]));
+    }, []);
+  
+    /**
+     * todos ステートが更新されたら、その値を保存
+    */
+    useEffect(() => {
+      localforage.setItem('todo-20200101', todos);
+    }, [todos]);
+
   return (
     <ThemeProvider theme={theme}>
-      <GlobalStyles styles={{ body: { margin: 0, padding: 0 } }} />
-      <ToolBar filter={filter} onToggleDrawer={handleToggleDrawer} />
-      <SideBar
-        drawerOpen={drawerOpen}
-        onFilter={handleFilter}
-        onToggleDrawer={handleToggleDrawer}
-      />
+      <GlobalStyles styles={{ body: {margin :0, padding: 0 } }} />
+      <ToolBar filter={filter} onToggleDrawer={handleToggleDrawer}/>
+      <SideBar drawerOpen={drawerOpen} onToggleDrawer={handleToggleDrawer} onFilter={handleFilter} />
       <FormDialog text={text} onChange={handleChange} onSubmit={handleSubmit} />
       <TodoItem todos={todos} filter={filter} onTodo={handleTodo} />
       <ActionButton todos={todos} onEmpty={handleEmpty} />
